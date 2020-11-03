937bb250c12e8f5720a908db6ea43531751f0489
Takafumi Arakaki <aka.tkf@gmail.com>
2020-11-02 22:05:06 -0500

    Pass-through kwargs to code_llvm/native(bookmark)

--- src/cthulhu.jl
+++ src/cthulhu.jl
@@ -1,18 +1,19 @@
-function CodeX.llvm(b::Cthulhu.Bookmark)
+function CodeX.llvm(b::Cthulhu.Bookmark; kw...)
     s = sprint() do io
         code_llvm(
             io,
             b;
             config = Cthulhu.CthulhuConfig(enable_highlighter = false),
             dump_module = true,
+            kw...,
         )
     end
     return CodeX.from_llvm(s)
 end
 
-function CodeX.native(b::Cthulhu.Bookmark)
+function CodeX.native(b::Cthulhu.Bookmark; kw...)
     s = sprint() do io
-        code_native(io, b; config = Cthulhu.CthulhuConfig(enable_highlighter = false))
+        code_native(io, b; config = Cthulhu.CthulhuConfig(enable_highlighter = false), kw...)
     end
     return CodeX.from_native(s)
 end
